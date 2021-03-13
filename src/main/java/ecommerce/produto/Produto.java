829664ee3cf072fac08b33c59350f6a6b2121ca3
Alife <contato@tifire.com>
2021-03-12 23:53:09 -0300

    utilizando a anotation @column para os atributos da classe produto

--- src/main/java/ecommerce/produto/Produto.java
+++ src/main/java/ecommerce/produto/Produto.java
@@ -1,7 +1,10 @@
 package ecommerce.produto;
 import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
 import ecommerce.base.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +15,10 @@ import lombok.Setter;
 @Table(name="produto")
 public class Produto extends BaseEntity{
 	
+	@Column(name="DESCRICAO")
 	private String descricao;
+	
+	@Column(name="VALVEN")
 	private BigDecimal valven;
 
 }
