beff8d20f13827c0fb858e0036ea0256ec63220f
Antonio Carlos <antoniohonorato19@gmail.com>
2021-03-19 00:38:38 -0300

    adição da variável descrição na classe categoria

--- src/main/java/ecommerce/categoria/Categoria.java
+++ src/main/java/ecommerce/categoria/Categoria.java
@@ -1,5 +1,19 @@
 package ecommerce.categoria;
 
-public class Categoria {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="categoria")
+public class Categoria {
+	
+	@Column(name="DESCRICAO")
+	private String descricao;
+	
 }
