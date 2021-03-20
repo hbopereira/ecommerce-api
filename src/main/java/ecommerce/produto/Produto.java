4d9975b4f1a11d4c0d83a7f0ad06a19b4c1f498a
Alife <contato@tifire.com>
2021-03-20 13:06:34 -0300

    adicionando associação manytomany unidirecional entre produto e cor

--- src/main/java/ecommerce/produto/Produto.java
+++ src/main/java/ecommerce/produto/Produto.java
@@ -1,11 +1,16 @@
 package ecommerce.produto;
 import java.math.BigDecimal;
-
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-
 import ecommerce.base.BaseEntity;
+import ecommerce.cor.Cor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +19,16 @@ import lombok.Setter;
 @Setter
 @Table(name="produto")
 public class Produto extends BaseEntity{
+	
+    @ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="produto_cor", joinColumns=
+	{@JoinColumn(name="produto_id")}, inverseJoinColumns=
+	{@JoinColumn(name="cor_id")})
+	private Set<Cor> cores;
+    
+	public Produto() {
+		cores = new HashSet<Cor>();
+	}
 	
 	@Column(name="DESCRICAO")
 	private String descricao;
